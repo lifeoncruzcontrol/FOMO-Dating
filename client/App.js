89ba72ed289e45b9142f5738149fbdf752f5ad83
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2022-02-02 09:00:19 -0800

    #4 - Created the Stack Navigator

--- client/App.js
+++ client/App.js
@@ -1,12 +1,19 @@
- import React, {useState} from 'react';
- import { View, Image, Text, Pressable, StyleSheet, Dimensions } from 'react-native';
+import React, {useState} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import LandingPage from './components/landingPage';
- 
- const App = () => {
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return(
-    <LandingPage/>
-   );
- };
- 
- export default App;
- 
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={LandingPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
