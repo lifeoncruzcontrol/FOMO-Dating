68a2cbaa261929010acabc5ecfb52931c1240314
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2022-02-02 10:31:09 -0800

    #4 - Overwritting styles of header

--- client/components/createAccount.js
+++ client/components/createAccount.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Dimensions } from 'react-native';
+
+import Header from './header';
 
 const CreateAccount = () => {
+  const viewStyle = {
+    marginTop: Dimensions.get('window').height / 9
+  };
+  const titleStyle = {
+    fontSize: 20,
+    marginBottom: 200
+  };
   return(
-    <Text>Create Account page</Text>
+    <>
+      <Header viewStyle={viewStyle} titleStyle={titleStyle}/>
+      <Text>Please enter your email for verification purposes:</Text>
+    </>
   );
 };
 
