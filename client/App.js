f62ad85d9a3ba1b6df8b31118b6435460b2a0715
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2022-02-02 09:18:29 -0800

    #4 - Added sample CreateAccount page for testing Navigator. Set initialRoute as Home

--- client/App.js
+++ client/App.js
@@ -3,14 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LandingPage from './components/landingPage';
+import CreateAccount from './components/createAccount';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return(
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={LandingPage} />
+        <Stack.Screen name="CreateAccount" component={CreateAccount} />
       </Stack.Navigator>
     </NavigationContainer>
   );
