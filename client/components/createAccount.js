ad7a3faf05e2df2010c69aec4dc86a0d3d32e642
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2022-02-02 09:19:18 -0800

    #4 - Sample CreateAccount page for testing

---
+++ client/components/createAccount.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const CreateAccount = () => {
+  return(
+    <Text>Create Account page</Text>
+  );
+};
+
+export default CreateAccount;
