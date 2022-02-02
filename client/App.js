4245b55a8bc91c114d44e22ccfed5c64c5a617a2
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2022-02-02 08:53:22 -0800

    #4 - Refactoring LandingPage into its respective component for native stack navigator

--- client/App.js
+++ client/App.js
@@ -1,84 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
  import React, {useState} from 'react';
  import { View, Image, Text, Pressable, StyleSheet, Dimensions } from 'react-native';
+import LandingPage from './components/landingPage';
  
  const App = () => {
-   const logo = "http://getdrawings.com/images/simple-mountain-drawing-21.jpg";
   return(
-    <View 
-      style={{
-        marginTop: Dimensions.get('window').height / 4,
-      }}
-    >
-      <View>
-        <Text
-          style={styles.title}
-        >
-          FOMO Dating
-        </Text>
-      </View>
-      <View>
-        <Pressable
-          style={styles.createAcct}
-        >
-          <Text
-            style={styles.text}
-          >
-            Create Account
-          </Text>
-        </Pressable>
-        <Pressable
-          style={styles.signIn}
-        >
-          <Text
-            style={styles.text}
-          >
-            Sign-in
-          </Text>
-        </Pressable>
-        <Text>
-          By creating an account or signing in, you agree to our Terms. Learn how we process your data in our Privacy Policy and Cookies Policy.
-        </Text>
-      </View>
-     </View>
+    <LandingPage/>
    );
  };
- 
- const styles = StyleSheet.create({
-   title: {
-    fontSize: 20,
-    alignSelf: 'center',
-    marginBottom: 200
-   },
-   createAcct: {
-     backgroundColor: '#add8e6',
-     width: "50%",
-     alignItems: 'center',
-     alignSelf: 'center',
-     paddingVertical: 20,
-     borderRadius: 10,
-     marginBottom: 20
-   },
-   signIn: {
-    backgroundColor: '#add8e6',
-    width: "50%",
-    alignItems: 'center',
-    alignSelf: 'center',
-    paddingVertical: 20,
-    borderRadius: 10,
-    marginBottom: 50
-  },
-   text: {
-     color: 'white'
-   }
- });
  
  export default App;
  
---
+++ client/components/landingPage.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { View, Text, Pressable, StyleSheet, Dimensions } from 'react-native';
+
+const LandingPage = () => {
+  return(
+    <View 
+      style={{
+        marginTop: Dimensions.get('window').height / 4,
+      }}
+    >
+      <View>
+        <Text
+          style={styles.title}
+        >
+          FOMO Dating
+        </Text>
+      </View>
+      <View>
+        <Pressable
+          style={styles.createAcct}
+        >
+          <Text
+            style={styles.text}
+          >
+            Create Account
+          </Text>
+        </Pressable>
+        <Pressable
+          style={styles.signIn}
+        >
+          <Text
+            style={styles.text}
+          >
+            Sign-in
+          </Text>
+        </Pressable>
+        <Text>
+          By creating an account or signing in, you agree to our Terms. Learn how we process your data in our Privacy Policy and Cookies Policy.
+        </Text>
+      </View>
+     </View>
+   );
+};
+
+const styles = StyleSheet.create({
+  title: {
+   fontSize: 20,
+   alignSelf: 'center',
+   marginBottom: 200
+  },
+  createAcct: {
+    backgroundColor: '#add8e6',
+    width: "50%",
+    alignItems: 'center',
+    alignSelf: 'center',
+    paddingVertical: 20,
+    borderRadius: 10,
+    marginBottom: 20
+  },
+  signIn: {
+   backgroundColor: '#add8e6',
+   width: "50%",
+   alignItems: 'center',
+   alignSelf: 'center',
+   paddingVertical: 20,
+   borderRadius: 10,
+   marginBottom: 50
+ },
+  text: {
+    color: 'white'
+  }
+});
+
+export default LandingPage;
