35b7d0dfb5745ed57b29b2871d4fc2c73e78b39c
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2022-02-08 15:47:58 -0800

    #4 - Completed 'Create Account' page

--- client/components/createAccount.js
+++ client/components/createAccount.js
@@ -1,22 +1,62 @@
-import React from 'react';
-import { Text, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import { Text, Dimensions, SafeAreaView, TextInput, Pressable, StyleSheet } from 'react-native';
 
 import Header from './header';
 
 const CreateAccount = () => {
+  const [email, setEmail] = useState(null);
   const viewStyle = {
-    marginTop: Dimensions.get('window').height / 9
+    marginTop: 0
   };
   const titleStyle = {
     fontSize: 20,
-    marginBottom: 200
+    marginBottom: 100
+  };
+  const inputStyle = {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
   };
   return(
     <>
-      <Header viewStyle={viewStyle} titleStyle={titleStyle}/>
-      <Text>Please enter your email for verification purposes:</Text>
+      <SafeAreaView>
+        <Header viewStyle={viewStyle} titleStyle={titleStyle}/>
+        <Text style={{alignSelf: 'center', fontSize: 30, marginBottom: 20}}>Please enter your email for verification purposes:</Text>
+        <TextInput
+          style={inputStyle}
+          placeholder='email@domain.com'
+          keyboardType='email-address'
+          onChangeText={setEmail}
+        />
+        <Pressable
+          style={styles.button}
+          onPress={() => console.log(email)}
+        >
+          <Text
+            style={styles.text}
+          >
+            Submit
+          </Text>
+        </Pressable>
+      </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#add8e6',
+    width: "50%",
+    alignItems: 'center',
+    alignSelf: 'center',
+    paddingVertical: 20,
+    borderRadius: 10,
+    marginBottom: 50
+  },
+  text: {
+    color: 'white'
+  }
+});
 
 export default CreateAccount;
