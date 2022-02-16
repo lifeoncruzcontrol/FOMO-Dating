829ea6da2a0badd235b8de503f915cba7ef15c34
Chris Cruz <41966126+chriscruz-1@users.noreply.github.com>
2022-02-15 16:16:47 -0800

    Added EnterVerificationCode and BuildProfile to Stack Navigator

--- client/FomoDating/App.js
+++ client/FomoDating/App.js
@@ -4,7 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './components/home';
 import EmailVerification from './components/createAccount/emailVerification';
+import EnterVerificationCode from './components/createAccount/enterVerificationCode';
 import GetUsernamePass from './components/createAccount/getUsernamePass';
+import BuildProfile from './components/createAccount/buildProfile';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,7 +16,9 @@ const App = () => {
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Email Verification" component={EmailVerification} />
+        <Stack.Screen name="Enter Verification Code" component={EnterVerificationCode} />
         <Stack.Screen name="Username/Password" component={GetUsernamePass} />
+        <Stack.Screen name="Build Profile" component={BuildProfile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
